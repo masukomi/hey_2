18bf8c7b4f175c433930f0e2b958c78c25947e19
masukomi <masukomi@masukomi.org>
2017-11-26 15:13:29 -0500

    added test db and referenced from spec helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,4 +1,7 @@
-ENV["DATABASE_URL"]="sqlite3:/Users/masukomi/Dropbox/apps/hey/database/hey.test.db"
+# ENV["DATABASE_URL"]="sqlite3:/Users/masukomi/Dropbox/apps/hey/database/hey.test.db"
+ENV["DATABASE_URL"]="sqlite3:/Users/masukomi/workspace/hey_2/spec/test_copy.db"
+
+`cp spec/test_seed.db spec/test_copy.db`
 
 require "spec"
 require "../src/hey"
