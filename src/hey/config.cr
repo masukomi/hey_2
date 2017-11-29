de019ab82c6141aaffaab4d9f533e6c78b066023
masukomi <masukomi@masukomi.org>
2017-11-28 22:36:59 -0500

    config now loads db url into env (breaks if no config or db file)

--- src/hey/config.cr
+++ src/hey/config.cr
@@ -1,24 +1,37 @@
 require "json"
 require "./interrupt_database"
-class Config
-	CONFIG_PATH="#{ENV["HOME"]}/.config/hey/config.json"
-	getter db_path : String
-	def initialize()
-		@db_path = find_db_path()
-	end
-	def find_db_path() : String
-		return "BULLSHIT PLACEHOLDER"
-	end
+module Hey
+	class Config
+		CONFIG_PATH="#{ENV["HOME"]}/.config/hey/config.json"
+		getter :db_path
+		def initialize()
+			@db_path = default_db_path()
+			update_db_env(@db_path)
+		end
+		
+		def default_db_path() : String
+			File.dirname(CONFIG_PATH) + "/hey.db"
+		end
+		def set_db_path(new_db_path : String)
+			@db_path = new_db_path
+			update_db_env(@db_path)
+		end
+		def update_db_env(db_path)
+			ENV["DATABASE_URL"] = "sqlite3:#{db_path}"
+		end
 
-	def self.load() : Config
-		return Config.from_json(File.read(CONFIG_PATH))
-	end
+		def save()
+			self.to_json(File.new(@db_path))
+		end
 
-	def interrupt_database()
-		return InterruptDatabase.new(self)
-	end
+		def self.load() : Config
+			config = Config.from_json(File.read(CONFIG_PATH))
+			config.update_db_env(config.db_path)
+			config
+		end
 
-	JSON.mapping({
-		db_path:  {type: String, nilable: false}
-	})
+		JSON.mapping({
+			db_path:  {type: String, nilable: true}
+		})
+	end
 end
