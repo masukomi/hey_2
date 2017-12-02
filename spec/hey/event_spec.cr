6a3ec6cf516b502b89b49bce4e5cfca42344c07c
masukomi <masukomi@masukomi.org>
2017-12-02 13:38:34 -0500

    Event#list_recent is real + Event.find_and_tag added
    crystal_fmt shard was added to support list_recent

--- spec/hey/event_spec.cr
+++ spec/hey/event_spec.cr
@@ -1,86 +1,90 @@
 require "../spec_helper"
 describe Hey::Event do
-	# it "should be initializable with a description" do
-	# 	e = Hey::Event.new(description: "described")
-	# 	e.should(be_a(Hey::Event))
-	# 	e.description.should(eq("described"))
-	# end
-    #
-	# it "should be able to load from the db" do
-	# 	e = Hey::Event.find(1)
-	# 	e.should(be_a(Hey::Event))
-	# 	if !e.nil?
-	# 		e.id.should(eq(1))
-	# 		# e.description.should(be_falsey()) 
-	# 		# currently descriptions aren't supported so... 
-	# 	end
-	# end
-    #
-	# it "should set a timestamp upon creation" do
-	# 	e = Hey::Event.new()
-	# 	saved = e.save
-	# 	created = e.created_at
-	# 	if saved
-	# 		e.destroy
-	# 	end
-	# 	/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/.should(be_truthy())
-	# end
-	# it "should have associated EventPerson objects" do
-	# 	e = Hey::Event.find(1)
-	# 	if !e.nil? 
-	# 		(e.event_persons.size > 0).should(be_true())
-	# 	end
-	# end
-    #
-	# it "should have associated people through EventPerson" do
-	# 	e = Hey::Event.find(1)
-	# 	if !e.nil? 
-	# 		(e.event_persons.size > 0).should(be_true())
-	# 		(e.persons.size > 0).should(be_true())
-	# 	end
-	# end
-	# it "should have associated tags through EventTag" do
-	# 	e = Hey::Event.find(1)
-	# 	if !e.nil? 
-	# 		(e.event_tags.size > 0).should(be_true())
-	# 		(e.tags.size > 0).should(be_true())
-	# 	end
-	# end
-    #
-	# it "should be able to load the last item" do
-	# 	# extension to granite orm
-	# 	e = Hey::Event.last()
-	# 	if !e.nil?
-	# 		#yay
-	# 		e.id.should(eq(2))
-	# 	else
-	# 		fail("unable to find via last method")
-	# 	end
-	# end
-    #
-	# it "should be able to find_by_last_or_id with last keyword" do
-	# 	e = Hey::Event.find_by_last_or_id("last")
-	# 	if !e.nil?
-	# 		e.id.should(eq(2))
-	# 	else
-	# 		fail("unable to find event by last keyword")
-	# 	end
-	# end
-    #
-	# it "should be able to find_by_last_or_id with string id" do
-	# 	e = Hey::Event.find_by_last_or_id("2")
-	# 	if !e.nil?
-	# 		e.id.should(eq(2))
-	# 	else
-	# 		fail("unable to find event by string id")
-	# 	end
-	# end
-    #
-	# it "should blow up find_by_last_or_id with bs id" do
-	# 	expect_raises Exception do
-	# 		e = Hey::Event.find_by_last_or_id("bullshit")
-	# 	end
-	# end
+	it "should be initializable with a description" do
+		e = Hey::Event.new(description: "described")
+		e.should(be_a(Hey::Event))
+		e.description.should(eq("described"))
+	end
+
+	it "should be able to load from the db" do
+		e = Hey::Event.find(1)
+		e.should(be_a(Hey::Event))
+		if !e.nil?
+			e.id.should(eq(1))
+			# e.description.should(be_falsey()) 
+			# currently descriptions aren't supported so... 
+		end
+	end
+
+	it "should set a timestamp upon creation" do
+		e = Hey::Event.new()
+		saved = e.save
+		created = e.created_at
+		if saved
+			e.destroy
+		end
+		created.nil?.should(be_false())
+		if created
+			(created == "").should(be_false())
+			/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/.match(created).should(be_truthy())
+		end
+	end
+	it "should have associated EventPerson objects" do
+		e = Hey::Event.find(1)
+		if !e.nil? 
+			(e.event_persons.size > 0).should(be_true())
+		end
+	end
+
+	it "should have associated people through EventPerson" do
+		e = Hey::Event.find(1)
+		if !e.nil? 
+			(e.event_persons.size > 0).should(be_true())
+			(e.persons.size > 0).should(be_true())
+		end
+	end
+	it "should have associated tags through EventTag" do
+		e = Hey::Event.find(1)
+		if !e.nil? 
+			(e.event_tags.size > 0).should(be_true())
+			(e.tags.size > 0).should(be_true())
+		end
+	end
+
+	it "should be able to load the last item" do
+		# extension to granite orm
+		e = Hey::Event.last()
+		if !e.nil?
+			#yay
+			e.id.should(eq(2))
+		else
+			fail("unable to find via last method")
+		end
+	end
+
+	it "should be able to find_by_last_or_id with last keyword" do
+		e = Hey::Event.find_by_last_or_id("last")
+		if !e.nil?
+			e.id.should(eq(2))
+		else
+			fail("unable to find event by last keyword")
+		end
+	end
+
+	it "should be able to find_by_last_or_id with string id" do
+		e = Hey::Event.find_by_last_or_id("2")
+		if !e.nil?
+			e.id.should(eq(2))
+		else
+			fail("unable to find event by string id")
+		end
+	end
+
+	it "should blow up find_by_last_or_id with bs id" do
+		expect_raises Exception do
+			e = Hey::Event.find_by_last_or_id("bullshit")
+		end
+	end
 	it "should know its table name" do
 		Hey::Event.table_name.should(eq("events"))
 	end
@@ -110,6 +114,27 @@ describe Hey::Event do
 			end
 		end
 		
+	end
+	it "should tag last with strings" do
+		initial_count = Hey::Event.count
+		event = Hey::Event.new()
+		event.save
+
+		Hey::Event.count.should(eq(initial_count + 1))
+		event.persons = [Hey::Person.first].compact
+		
+		event = Hey::Event.last
+		if event 
+			# TODO it's getting the wrong event despite finding 
+			# .last with ORDER BY created_at DESC LIMIT 1
+			event.tags.should(eq(Array(Hey::Tag).new))
+
+			Hey::Event.find_and_tag("last", ["alpha", "beta"])
+			event = Hey::Event.last
+			if event
+				event.tags.size.should(eq(2))
+			end
+		end
 	end
 
 
--- src/hey/event.cr
+++ src/hey/event.cr
@@ -5,6 +5,7 @@ require "../granite_orm/fields.cr"
 require "../granite_orm/transactions.cr"
 require "../granite_orm/querying.cr"
 # require "../granite_orm/adapter/sqlite.cr"
+require "crystal_fmt"
 
 module Hey
 	class Event < Granite::ORM::Base
@@ -27,12 +28,31 @@ module Hey
 		field created_at : String
 
 		before_create :set_created_at
-
-		def self.list_recent(limit : Int32)
-			puts "todo: output <= #{limit} recent events"
+		
+		# Outputs a table of recent events:
+		# Example:
+		# ID | Who       | When                 | Tags
+		# 2. | Bob, Mary | 4/12/17 14:23        | meeting, scheduled
+		# 3. | Bob       | 4/12/17 14:26        |
+		# 4. | Sam       | 4/12/17 16:11        | question 
+		# 5. | Mary      | 4/12/18 09:22        | task list
+		def self.list_recent(limit : Int32 = 25)
+			recent = Event.all("ORDER BY #{@@order_column} LIMIT #{limit}")
+			data =  Array(Array(String|Nil)).new
+			data << ["ID", "Who", "When", "Tags"]
+			recent.each do | event |
+				data << [
+					event.id.to_s,
+					event.persons.map{|p|p.name}.join(", "),
+					event.created_at, # is a string in sqlite
+					event.tags.map{|t|t.name}.join(", ")
+				]
+			end
+			t = Table.new(data)
+			puts t.format
 		end
 		
-		def self.find_by_last_or_id(identifier : String)
+		def self.find_by_last_or_id(identifier : String) : Event?
 			event : Event?
 			if identifier == "last"
 				event = Event.last()
@@ -45,12 +65,48 @@ module Hey
 
 		def set_created_at
 			# 2017-05-26 17:33:08
-			created_at = Time.now().to_s("%Y-%m-%d %H:%M:%S")
+			self.created_at = Time.now().to_s("%Y-%m-%d %H:%M:%S")
 		end
 
 		def add_tags(new_tags : Array(Hey::Tag))
 			addable = new_tags - tags
 			self.tags = (self.tags + addable)
+		end
+
+		def self.find_and_tag(last_or_id : String, tag_strings : Array(String)) : Bool
+			event = Event.find_by_last_or_id(last_or_id)
+			if event
+				tags = tag_strings.map{ |string|
+					t = Tag.find_by :name, string
+					if t.nil?
+						t = Tag.new()
+						t.name = string
+						t.save
+					end
+					t
+				}
+				event.add_tags(tags)
+				return true
+			else
+				STDERR.puts("Unable to find Event with that identifier: #{last_or_id}")
+				return false
+			end
+		end
+		#-----------------------------------
+		def self.command_proc() : Proc(Array(String), Bool)
+			Proc(Array(String), Bool).new{ |args|
+					limit = 25
+					if args.size > 0
+						limit = args.first.to_i
+					end
+					Event.list_recent(limit)
+					true
+				}
+		end
+		def self.command_description() : String
+"  hey list [number] 
+     lists recent events
+     defaults to 25"
 		end
 	end
 end
