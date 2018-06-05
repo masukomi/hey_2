11a9c928b5db2bc4c3d9d71378ae32fb3782614b
masukomi <masukomi@masukomi.org>
2018-06-05 00:21:44 -0400

    reworked build script in prep of homebrew

--- build.sh
+++ build.sh
@@ -1,18 +1,40 @@
 #!/bin/sh
+VERSION="dev_version"
+if [ "$1" != "" ]; then 
+  VERSION=$1
+  perl -pi -e "s/VERSION_NUMBER_HERE/$1/" src/hey.cr
+fi
+crystal build --release src/hey.cr
+if [ "$1" != "" ]; then 
+  perl -pi -e "s/$1/VERSION_NUMBER_HERE/" src/hey.cr
+fi
 
-crystal build src/hey.cr
-crystal build src/hey/reports/people_report.cr
-crystal build src/hey/reports/interrupts_by_hour.cr
-crystal build src/hey/reports/sparkline_24.cr
 
-cp hey starter_files/
 
-# move reports
-cp people_report ~/.config/hey/reports/
-mv people_report starter_files/reports/
+crystal build --release src/hey/reports/people_report.cr
+crystal build --release src/hey/reports/interrupts_by_hour.cr
+crystal build --release src/hey/reports/sparkline_24.cr
 
-cp interrupts_by_hour ~/.config/hey/reports/
-mv interrupts_by_hour starter_files/reports/
+version_dir="hey_$VERSION"
+rm -rf $version_dir
+mkdir -p $version_dir/reports
+mkdir -p $version_dir/upgrade_scripts
+cp upgrade_scripts/* $version_dir/upgrade_scripts/
+cp hey $version_dir/
 
-cp sparkline_24 ~/.config/hey/reports/
-mv sparkline_24 starter_files/reports/
+
+# make sure the required directories exist
+mkdir -p ~/.config/hey/reports
+
+
+cp starter_files/hey.db $version_dir/hey.db
+mkdir -p $version_dir/reports
+cp people_report $version_dir/reports/
+cp interrupts_by_hour $version_dir/reports/
+cp sparkline_24 $version_dir/reports/
+
+# compress version dir
+tar -czf $version_dir.tgz $version_dir
+rm -rf $version_dir
+
+
