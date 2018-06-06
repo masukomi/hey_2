72e181d1b5c897203c7760c4fb0317dd951f3a77
masukomi <masukomi@masukomi.org>
2018-06-06 19:21:29 -0400

    removed unnecessary cli_tool methods from changelog manager

--- src/hey/cli_tool.cr
+++ src/hey/cli_tool.cr
@@ -37,78 +37,13 @@ module Hey::CliTool
 		return input
 	end
 
-	def get_change_type(quit_on_none = true) : String
-		question_string = get_change_type_questions_string()
-		answers = get_change_type_answers_list
-		int_change_type = ask_until_acceptable(
-			question_string,
-			answers
-		).to_i
-		# quit if reqested
-		if int_change_type == answers.last.to_i
-			if quit_on_none
-				exit 0
-			else
-				return "Skip"
-			end
-		end
-		#
-		return ChangelogEntry::CHANGE_TYPES_HASH[int_change_type]
-	end
-
 	def convert_comma_list_to_array(list : String) : Array(String)
 		return list.to_s.split(/\s*,\s*/).select{|x| !x.nil? && x != ""}
 		# i don't trust them to follow instructions
 	end
 
-	def get_change_type_answers_list() : Array(String)
-		cth = ChangelogEntry::CHANGE_TYPES_HASH
-		sorted_keys = cth.keys.sort[0..(cth.keys.size - 1)]
-		# - 2 because the last key is for internal use only
-		# but + 1 because we want an exta one for the quit option
-		# so - 1
-		answers = sorted_keys.map{|x| x.to_s}
-		answers.push((cth.keys.size + 1).to_s)
-		return answers
-	end
-	def get_change_type_questions_string() : String
-		question_string = "What kind of Change?"
-		cth = ChangelogEntry::CHANGE_TYPES_HASH
-		cth.keys.sort.each do |number|
-			question_string += "\n#{number} - #{cth[number]}"
-		end
-
-		# allow users to quit
-		quit_number = cth.keys.size + 1
-		question_string += "\n#{quit_number} - Quit. No entry please."
-		question_string += "\n#{cth.keys.sort.push(quit_number)}: "
-		return question_string
-	end
-
-	def get_changelog_db(called_from : String) : ChangelogDatabase
-		cd = ChangelogDatabase.new(called_from)
-		if cd.dot_changelog_entries_path().nil?
-			prep_entries_directory(called_from, cd)
-		end
-		return cd
-	end
-
 	def get_called_from() : String
 		called_from = File.expand_path(".").to_s
-	end
-
-	def prep_entries_directory(called_from : String, cd : ChangelogDatabase)
-		puts "I couldn't find a .changelog_entries directory here"
-		puts "\t#{called_from}"
-		puts "or in any directory above it."
-		can_create = ask_yes_no( "Can I create it there?")
-		if can_create
-			cd.create_default_folder()
-			puts "I've created it. Thanks.\n------------------"
-		else
-			puts "I can't continue without that. Sorry."
-			exit 1
-		end
 	end
 
 	def get_stdin_from_tty()
