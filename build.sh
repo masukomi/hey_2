c6b8c08ff35360dcbf11b206dc28e71e8aed29d5
masukomi <masukomi@masukomi.org>
2018-06-06 22:00:11 -0400

    reorganized build.sh

--- build.sh
+++ build.sh
@@ -22,9 +22,15 @@ crystal build --release src/hey/reports/sparkline_24.cr
 echo "copying files around for distribution..."
 version_dir="hey_$VERSION"
 rm -rf $version_dir
+
 mkdir -p $version_dir/reports
+cp people_report $version_dir/reports/
+cp interrupts_by_hour $version_dir/reports/
+cp sparkline_24 $version_dir/reports/
+
 mkdir -p $version_dir/upgrade_scripts
 cp upgrade_scripts/* $version_dir/upgrade_scripts/
+
 cp hey $version_dir/
 
 
@@ -33,10 +39,6 @@ mkdir -p ~/.config/hey/reports
 
 
 cp starter_files/hey.db $version_dir/hey.db
-mkdir -p $version_dir/reports
-cp people_report $version_dir/reports/
-cp interrupts_by_hour $version_dir/reports/
-cp sparkline_24 $version_dir/reports/
 
 # compress version dir
 echo "tarring it up as $version_dir.tgz"
