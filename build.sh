89c91f117f7d34097a0a248a117bc1601b185774
masukomi <masukomi@masukomi.org>
2019-01-12 19:12:30 -0500

    build.sh now checks if starter db has changed
    if it has it provides a checklist of the various related tasks
    that must be performed before releasing the next version.

--- build.sh
+++ build.sh
@@ -10,33 +10,17 @@ if [ "$1" != "" ]; then
   perl -pi -e "s/$1/VERSION_NUMBER_HERE/" src/hey/version.cr
 fi
 
-
-
-echo "building people_report..."
-crystal build --release src/hey/reports/people_report.cr
-echo "building interrupts_by_hour..."
-crystal build --release src/hey/reports/interrupts_by_hour.cr
-echo "building sparkline_24..."
-crystal build --release src/hey/reports/sparkline_24.cr
-
 echo "copying files around for distribution..."
 version_dir="hey_v$VERSION"
 rm -rf $version_dir
-
-mkdir -p $version_dir/reports
-cp people_report $version_dir/reports/
-cp interrupts_by_hour $version_dir/reports/
-cp sparkline_24 $version_dir/reports/
 
 mkdir -p $version_dir/upgrade_scripts
 cp upgrade_scripts/* $version_dir/upgrade_scripts/
 
 cp hey $version_dir/
 
-
 # make sure the required directories exist
 mkdir -p ~/.config/hey/reports
-
 
 cp starter_files/hey.db $version_dir/hey.db
 
@@ -44,8 +28,21 @@ cp starter_files/hey.db $version_dir/hey.db
 echo "tarring it up as $version_dir.tgz"
 tar -czf $version_dir.tgz $version_dir
 rm -rf $version_dir
-sha=$(shasum -a 256 $version_dir.tgz)
-echo "release sha is $sha"
-echo "DONE"
 
+db_sha=$(shasum -a 256 starter_files/hey.db | sed -e "s/ .*//")
+if [ "$db_sha" != "88fe5ed82c33da62bbd9790722e0ff025da96a906aa20b761b94d307d1966264" ]; then
+  echo "WARNING: starter db has been updated." 
+  echo "You must:"
+  echo " * upload the updated hey.db to the starer_files dir"
+  echo " * update the sha in db_setup.sh to $sha"
+  echo " * upload the updated db_setup.sh"
+  echo " * create/update and upload the db upgrade script"
+  echo " * update the sha in build.sh once all of that has been done"
+else
+  echo "starter db is unchanged."
+fi
+
+sha=$(shasum -a 256 $version_dir.tgz)
+echo "homebrew release sha is $sha"
+echo "DONE"
 
